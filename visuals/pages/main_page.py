58cf77cb3ebef5baa33dac3b154d42f93774498a
Astley <aslbekxamidov2007@gmail.com>
2025-05-24 11:09:00 +0500

    added a map to main page

--- visuals/pages/main_page.py
+++ visuals/pages/main_page.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.write("hello")
+import numpy as np
+import pandas as pd
+import time
+
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [10, 10] + [41.3111, 69.2797],
+    columns=['lat', 'lon']
+)
+
+st.map(map_data)
