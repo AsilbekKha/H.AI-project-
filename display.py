8cef325d5aee46bc7a0924547705fe12f424a3c9
Astley <aslbekxamidov2007@gmail.com>
2025-05-24 13:03:44 +0500

    new directions

--- display.py
+++ display.py
@@ -81,9 +81,9 @@ import time
 # st.button("Balloons!")
 # st.balloons()
 # Define the pages
-main_page = st.Page("pages/main_page.py", title="Main Page", icon="🎈")
-page_2 = st.Page("pages/page2.py", title="Page 2", icon="❄️")
-page_3 = st.Page("pages/page3.py", title="Page 3", icon="🎉")
+main_page = st.Page("visuals/pages/main_page.py", title="Main Page", icon="🎈")
+page_2 = st.Page("visuals/pages/page2.py", title="Page 2", icon="❄️")
+page_3 = st.Page("visuals/pages/page3.py", title="Page 3", icon="🎉")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3])
--- visuals/pages/main_page.py
+++ visuals/pages/main_page.py
@@ -3,10 +3,18 @@ import numpy as np
 import pandas as pd
 import time
 
-
+st.markdown(
+    """
+    <h2 style='text-align: center; color: #4CAF50; font-family: Arial;'>
+        Important map data is provided belove!
+    </h2>
+    """,
+    unsafe_allow_html=True
+)
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [10, 10] + [41.3111, 69.2797],
     columns=['lat', 'lon']
-)
-
+)s
+if st.button("Celebrate"):
+    st.balloons()
 st.map(map_data)
