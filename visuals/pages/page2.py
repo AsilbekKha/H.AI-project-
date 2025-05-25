91cbfa21e395fd6e0077b5ab0a49a80d99199177
Astley <aslbekxamidov2007@gmail.com>
2025-05-25 13:02:27 +0500

    rehearsal

--- visuals/pages/page2.py
+++ visuals/pages/page2.py
@@ -1,2 +1,27 @@
 import streamlit as st
-st.write("nigga")
+import sys
+import os
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+from main import load_data, preprocess_data
+
+st.set_page_config(page_title="Страница 2: Визуализация данных", layout="wide")
+st.title("Страница 2: Визуализация данных")
+
+visits, clients, _ = load_data()
+data = preprocess_data(visits, clients)
+
+st.subheader("Продолжительность визитов по дням")
+visit_duration = data[['Date', 'Visit_Duration']].sort_values('Date')
+visit_duration['Date'] = pd.to_datetime(visit_duration['Date'])
+daily_duration = visit_duration.groupby('Date')['Visit_Duration'].mean()
+st.line_chart(daily_duration)
+
+st.subheader("Количество визитов по дням недели")
+visits_per_weekday = data['Weekday'].value_counts().sort_index()
+st.bar_chart(visits_per_weekday)
+
+st.subheader("Средняя продолжительность визита по полу (в минутах)")
+avg_duration_gender = data.groupby('Gender')['Visit_Duration'].mean() / 60
+st.bar_chart(avg_duration_gender)
